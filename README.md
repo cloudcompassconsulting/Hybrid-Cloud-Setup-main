4176066f1b2bbd4514447cad63fd67e2e154f9e4
cloudcompassconsulting <131810203+cloudcompassconsulting@users.noreply.github.com>
2023-06-12 19:53:50 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Hybrid-Cloud-Setup-main
- This repository provides scripts to set up a WireGuard VPN connection between a local server (behind CGNAT) and a VPS server
+# Exposing Local Server Apps Behind CGNAT using WireGuard and iptables
+
+This repository provides scripts to set up a WireGuard VPN connection between a local server (behind CGNAT) and a VPS server. The setup allows the local server to expose applications running on specific ports to the internet, overcoming the limitations imposed by CGNAT.
+
+## Prerequisites
+
+- Linux-based local server with root access.
+- Linux-based VPS server with root access.
+
+## Server Setup
+
+The `wg_vps.sh` script automates the setup process on the VPS server. It performs the following steps:
+
+1. Installs WireGuard on the VPS server.
+2. Prompts for the WireGuard client IP address and the name of the interface connected to the internet (e.g., eth0).
+3. Enables IP forwarding and sets up iptables rules to allow traffic forwarding and port forwarding for the specified ports (e.g., 25, 80, 443).
+4. Configures WireGuard and generates the server and client configuration files.
+5. Sets up persistent iptables rules to load on boot.
+
+To run the script, execute the following commands on the VPS server: wg_vps.sh
+Follow the prompts and provide the necessary information to complete the setup.
+
+## Client Setup
+
+The `wg_client.sh` script automates the setup process on the local server. It performs the following steps:
+
+1. Installs WireGuard on the local server.
+2. Prompts for the VPS server's WireGuard configuration file path and the local server's WireGuard configuration file path.
+3. Enables IP forwarding and sets up iptables rules to allow traffic forwarding and port forwarding for the specified ports (configured on the VPS server).
+4. Configures WireGuard and loads the client configuration.
+
+To run the script, execute the following commands on the local server: wg_client.sh
---
+++ wg_vps.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Install WireGuard
+wget https://git.io/wireguard -O wireguard-install.sh
+bash wireguard-install.sh
+
+# store the default the network interface name
+NETWORK_INTERFACE=$(ip -4 route | awk '/default/ {print $5}' | head -n 1)
+PRIVATE_IP=$(ip -4 addr show $IFACE | awk '/inet/ {print $2}' | cut -d'/' -f1)
+
+# Ask the user for the client IP address
+read -p "Enter the client IP address: " CLIENT_IP
+
+# Assuming the wireguard interface is named wg0
+
+# Enable IP forwarding
+echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+sysctl -p
+
+# Flush iptables nat table and set default policies
+iptables -t nat -F
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+# Add iptables rules for ports 25, 80, and 443
+iptables -A INPUT -p tcp -m tcp --dport 25 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+# Forward and masquerade all traffic from ports 25, 80, and 443 to the client
+iptables -A FORWARD -i wg0 -o $NETWORK_INTERFACE -p tcp -m multiport --dports 25,80,443 -d $CLIENT_IP -j ACCEPT
+iptables -t nat -A POSTROUTING -o $NETWORK_INTERFACE -p tcp -m multiport --dports 25,80,443 -d $CLIENT_IP -j MASQUERADE
+
+# Define the ports you want to forward
+PORTS="25,80,443"
+
+# Add the PostUp and PostDown commands to the WireGuard configuration file
+echo "PostUp = iptables -t nat -A PREROUTING -p tcp -i $NETWORK_INTERFACE --match multiport --dports $PORTS -j DNAT --to-destination $CLIENT_IP" >> /etc/wireguard/wg0.conf
+echo "PostUp = iptables -t nat -A POSTROUTING -o $NETWORK_INTERFACE -j SNAT --to-source $PRIVATE_IP" >> /etc/wireguard/wg0.conf
+echo "PostDown = iptables -t nat -D PREROUTING -p tcp -i $NETWORK_INTERFACE --match multiport --dports $PORTS -j DNAT --to-destination $CLIENT_IP" >> /etc/wireguard/wg0.conf
+echo "PostDown = iptables -t nat -D POSTROUTING -o $NETWORK_INTERFACE -j SNAT --to-source $PRIVATE_IP" >> /etc/wireguard/wg0.conf
+
+# Restart WireGuard for changes to take effect
+wg-quick down wg0 && wg-quick up wg0
+
+# Save the iptables rules
+iptables-save > /etc/iptables.up.rules
+
+# Create a script that will load the iptables rules on boot
+echo "#!/bin/sh" > /etc/network/if-pre-up.d/iptables
+echo "/sbin/iptables-restore < /etc/iptables.up.rules" >> /etc/network/if-pre-up.d/iptables
+chmod +x /etc/network/if-pre-up.d/iptables
