b55539ec4976d3e67d58972f9d02d0bd12f80c50
cloudcompassconsulting <131810203+cloudcompassconsulting@users.noreply.github.com>
2024-11-17 22:12:20 -0500

    Update wireguard-client-iptables.sh
    clean up duplicate save and set rules before saving

--- vpn-setup/client/wireguard-client-iptables.sh
+++ vpn-setup/client/wireguard-client-iptables.sh
@@ -2,12 +2,13 @@
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
+# forward to the loadbalancer
+sudo iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.8.60:80 #Note the IP address of the load balancer is set to 192.168.8.60 on the router as a static hosts
+sudo iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 192.168.8.60:443
+
 # Finally, enable IP forwarding
 echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
-
-# Save the iptables rules
-sudo iptables-save | sudo tee /etc/iptables/rules.v4
 
 # Save the iptables rules
 sudo iptables-save > /etc/iptables/rules.v4
@@ -15,6 +16,4 @@ sudo iptables-save > /etc/iptables/rules.v4
 # Make sure the rules persist across reboots
 sudo apt-get install -y iptables-persistent
 
-# forward to the loadbalancer
-sudo iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.8.60:80 #Note the IP address of the load balancer is set to 192.168.8.60 on the router as a static hosts
-sudo iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 192.168.8.60:443
+
